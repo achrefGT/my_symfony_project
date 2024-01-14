2b54c53f5981b63b2a903dfa8a1e6b6d8111eb9d
achrefGT <achrefgataa@gmail.com>
2024-01-14 12:37:31 +0100

    Authentication : login/logout + comments

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,11 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
+use App\Entity\Comment;
 use App\Repository\ArticleRepository;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use App\Form\ArticleType;
+use App\Form\CommentType;
+
 use Doctrine\ORM\EntityManagerInterface;
 
 
@@ -76,9 +79,27 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show(Article $article){
+    public function show(Article $article, Request $request, EntityManagerInterface $manager){
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setCreatedAt(new \DateTime)
+                    ->setArticle($article);
+
+            $manager->persist($comment);
+            $manager->flush();
+            
+            return $this->redirectToRoute('blog_show', [
+                'id' => $article->getId()
+            ]);
+
+        }
+
         return $this->render('blog/show.html.twig', [
             'article' => $article,
+            'commentForm' => $form->createView()
         ]);
     }
 }
